d6c10de8edaf27ec956108a1d18ec9ebaeabb294
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-04 12:58:42 +0545

    added app bar which have  action button, profile image with text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,15 +20,36 @@ AppBar _buildAppBar() {
     title: Row(
       children: [
         Container(
+          margin: EdgeInsets.only(left: 10, top: 5),
           height: 45,
           width: 45,
           decoration: BoxDecoration(
             color: kLightGrey,
             borderRadius: BorderRadius.circular(10),
           ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset('assets/images/profile.png', fit: BoxFit.cover),
+          ),
+        ),
+        SizedBox(width: 10),
+        Text(
+          'Hello, Ram',
+          style: TextStyle(
+            color: kDark,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     ),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.more_vert, color: kDark, size: 30),
+      ),
+      SizedBox(width: 10),
+    ],
     centerTitle: true,
     backgroundColor: kWhite,
     elevation: 0,
